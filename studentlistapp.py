9b199eb80a6675de2730c302e0186b9ef9083c19
eran1789 <64136319+eran1789@users.noreply.github.com>
2020-04-22 12:30:08 +0200

    Update studentlistapp.py

--- studentlistapp.py
+++ studentlistapp.py
@@ -1,5 +1,5 @@
 studentlist = ["johson", "xxyy0011", "joso1122"]
 
-print("These students have finished the lab: ")
+print("These students have finished the lab: Anton Eriksson, Joel Tjernström")
 for student in studentlist:
   print(student)
